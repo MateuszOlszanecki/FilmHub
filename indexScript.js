21cd71ce483f99111d5672bd164f0a68ab2f89e8
MateuszOlszanecki <mateuszolszanecki@gmail.com>
2023-05-14 21:12:18 +0200

    MenuDone

--- indexScript.js
+++ indexScript.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const addFilm = document.querySelector('.addFilm')
+const listFilm = document.querySelector('.listFilm')
+
+addFilm.addEventListener('click', () => {
+    document.location.href = "addFilm.html"
+})
+
+listFilm.addEventListener('click', () => {
+    document.location.href = "listFilm.html"
+})
